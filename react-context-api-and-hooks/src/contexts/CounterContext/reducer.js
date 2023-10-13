33d202e24247382ec4621cdad3103a0a1faa3809
mrcelo014@gmail.com <mrcelo014@gmail.com>
2023-10-13 06:58:53 -0300

    reducer

---
+++ react-context-api-and-hooks/src/contexts/CounterContext/reducer.js
@@ -0,0 +1,25 @@
+import * as actionsTypes from '/react-context-api-and-hooks/src/contexts/CounterContext/actionTypes.js'
+import { initialState } from '/react-context-api-and-hooks/src/contexts/CounterContext/CounterContextProvider.js'
+
+
+export const reducer = (state, action) => {
+  
+  switch (action.type) {
+    case actionsTypes.INCREASE:
+      return { ...state, counter: state.counter + 1 }
+    case actionsTypes.DECREASE:
+      return { ...state, counter: state.counter - 1 }
+    case actionsTypes.RESET:
+      return { ...initialState }
+    case actionsTypes.SET_COUNTER:
+      return { ...state, ...action.payload }
+    case actionTypes.ASYNC_INCREASE_START:
+      return { ...state, loading: true }
+    case actionsTypes.ASYNC_INCREASE_END:
+      return { ...state, loading: false, counter: state.counter + 1 }
+    case actionTypes.INCREASE_ERROR:
+      return { ...state, loading: false }
+  }
+  
+  return state
+}
